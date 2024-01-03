99839528e0daa1cd6c973963bf8924509e769d0c
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-01-03 23:15:19 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,13 @@
+package udemy.com;
+
+public class variables {
+    public static void main(String[] args){
+        System.out.println("Java is strongly typed language");
+        int num1 = 8;
+        int num2 = 45;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
+
+
