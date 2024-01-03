3500c5d102dc8832227b243f7517f537c80534fb
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-01-03 23:49:59 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,22 @@
+package udemy.com;
+
+public class Datatypes {
+    public static void main(String[] args){
+        System.out.println("Java follows UNICODE and NOT a ASCII code");
+        int num = 19;
+        byte by = 100;
+        short sh = 554;
+        long lg = 456L;
+
+        float f = 456.1f;
+        double d = 456.43;
+
+        char ch = 'A';
+
+        boolean val = true;
+
+        System.out.println("Working Successfully");
+    }
+}
+
+
