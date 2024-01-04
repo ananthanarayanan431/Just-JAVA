c857ed39209eb361fbe1e42a0e15f13e52d3dc7c
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-01-04 13:14:51 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,27 @@
+package udemy.com;
+
+public class literals {
+    public static void main(String[] args){
+
+        int num1 = 0b101;
+        System.out.println(num1);
+
+        int num2 = 0x7E;
+        System.out.println(num2);
+
+        int num3 = 10_00_000;
+        System.out.println(num3);
+
+        double val1 = 56;
+        float val2 = 56f;
+        System.out.println(val2);
+        System.out.println(val1);
+
+        char ch ='a';
+        ch++;
+        System.out.println(ch);
+        System.out.println("Working Successfully");
+    }
+}
+
+
