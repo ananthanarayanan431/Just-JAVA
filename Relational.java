a17cb782a4cab5571cf2e86ad9a2a96c817be13b
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-01-05 12:47:45 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,21 @@
+package udemy.com;
+
+public class Relational {
+    public static void main(String[] args){
+
+        int x = 6;
+        int y = 8;
+
+        boolean result = x > y;
+        System.out.println(result);
+
+        boolean result1 = x==y;
+        System.out.println(result1);
+
+        boolean result3 = x<=y;
+        System.out.println(result3);
+        System.out.println("Working Successfully");
+    }
+}
+
+
