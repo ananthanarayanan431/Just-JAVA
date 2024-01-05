c73c7ded3e9b1c2ca4874bc691614254d93eb9e3
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-01-05 16:14:30 +0530

    Add files via upload

---
+++ switch_stat.java
@@ -0,0 +1,48 @@
+package udemy.com;
+
+public class switch_stat {
+    public static void main(String[] args){
+
+        int num=2;
+
+        switch (num) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Enter a valid number");
+        }
+
+//        switch (num){
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Enter a valid number");
+//        }
+        System.out.println("Working Successfully");
+    }
+}
+
+
