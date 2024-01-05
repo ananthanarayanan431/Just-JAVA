00f3742d1a3c391e414e06607ed8bd1fea028ad8
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-01-05 16:17:57 +0530

    Add files via upload

---
+++ Need_for.java
@@ -0,0 +1,15 @@
+package udemy.com;
+
+public class Need_for {
+    public static void main(String[] args){
+
+        int num=4;
+
+//        repeat something for 'n' no of times
+//        continue doing something for till the condition matches
+
+        System.out.println("Working Successfully");
+    }
+}
+
+
