6ae51672b8f2f44cfa42c5e4a060995a5e04e954
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-01-05 16:00:20 +0530

    Add files via upload

---
+++ Ifelseifelse.java
@@ -0,0 +1,23 @@
+package udemy.com;
+
+public class Ifelseifelse {
+    public static void main(String[] args){
+
+        int x=10;
+        int y=1;
+        int z=7;
+
+        if (x>y&&x>z){
+            System.out.println("X is greater number");
+        }
+        else if (y>z) {
+            System.out.println("Y is greater number");
+        }
+        else {
+            System.out.println("Z is greater");
+        }
+        System.out.println("Working Successfully");
+    }
+}
+
+
