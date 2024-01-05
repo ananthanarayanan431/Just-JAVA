233392e2c05a2b0202cdf76ad05b0579221df99f
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-01-05 15:56:46 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,19 @@
+package udemy.com;
+
+public class Ifelse {
+    public static void main(String[] args){
+        System.out.println("Java is not dependent on any indentation");
+
+        int x=11;
+
+        if (x>=5 && (x%2==0)) {
+            System.out.println("Hello World");
+        } else {
+            System.out.println("Bye");
+        }
+
+        System.out.println("Working Successfully");
+    }
+}
+
+
