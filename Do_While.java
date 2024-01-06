a7569e00a2b4af2cb109bf25ac9a633f0fbbcccf
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-01-06 20:53:04 +0530

    Add files via upload

---
+++ Do_While.java
@@ -0,0 +1,14 @@
+package udemy.com;
+
+public class Do_While {
+    public static void main(String[] args){
+
+        int i=5;
+        do {
+            System.out.println("Hi " + i);
+        } while (i<4);
+        System.out.println("Working Successfully");
+    }
+}
+
+
