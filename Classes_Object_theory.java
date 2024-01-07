e5044b07f1713f3fa775c8632c4e6099d57801b0
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-01-07 09:28:20 +0530

    Add files via upload

---
+++ Classes_Object_theory.java
@@ -0,0 +1,15 @@
+package udemy.com;
+
+public class Classes_Object_theory {
+    public static void main(String[] args){
+
+        int num=5;
+        System.out.println("Every object knows something and Do something");
+        System.out.println("Every Object -> Properties and Behaviour");
+        System.out.println("Classes acts as a Blueprint");
+        System.out.println("JVM creates a Object for you");
+        System.out.println("Working Successfully");
+    }
+}
+
+
