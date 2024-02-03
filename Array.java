e8c3010df4dbecedb5666c0fb20334b75fc1f133
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-02-03 14:27:26 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,13 @@
+package udemy.com;
+
+public class Array {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5};
+        System.out.println(arr[0]);
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars[1]);
+        System.out.println("Working Successfully");
+    }
+}
